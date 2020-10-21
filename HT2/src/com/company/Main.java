060726b5d2163c1c9c19e763e9e1d639c1296eb2
milkaalinka17 <alina.shumilova1704@gmail.com>
2020-10-21 04:32:01 +0300

    Task 3.1

--- HT2/src/com/company/Main.java
+++ HT2/src/com/company/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    /*System.out.println("Task 56");
         System.out.println("Enter a,b,c,x,y:");
 	    int a = Integer.parseInt(br.readLine());
 	    int b = Integer.parseInt(br.readLine());
@@ -20,6 +21,14 @@ public class Main {
         boolean d;
         if (((a*b) | (b*c) | (a*c)) <= (x*y)) d = true;
         else d = false;
-        System.out.println(d);
+        System.out.println(d);*/
+
+        System.out.println("Task 3");
+        System.out.println("Enter radius:");
+        int radius = Integer.parseInt(br.readLine());
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+        System.out.println("Perimetr = "+perimeter);
+        System.out.println("Area = "+area);
     }
 }
