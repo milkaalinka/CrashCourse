2b100bd0ca75bf3e9a0bb1da09861d15169d7e03
Alina <65493289+milkaalinka@users.noreply.github.com>
2020-10-21 05:02:40 +0300

    Update Main.java

--- HT2/src/com/company/Main.java
+++ HT2/src/com/company/Main.java
@@ -10,19 +10,6 @@ import static com.sun.tools.doclint.Entity.or;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    System.out.println("Task 56");
-        System.out.println("Enter a,b,c,x,y:");
-	    int a = Integer.parseInt(br.readLine());
-	    int b = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
-        int x = Integer.parseInt(br.readLine());
-        int y = Integer.parseInt(br.readLine());
-        boolean d;
-        if (((a*b) | (b*c) | (a*c)) <= (x*y)) d = true;
-        else d = false;
-        System.out.println(d);
-
         System.out.println("Task 3.1");
         System.out.println("Enter radius:");
         int radius = Integer.parseInt(br.readLine());
